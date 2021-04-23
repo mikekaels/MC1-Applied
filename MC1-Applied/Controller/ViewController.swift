1e219043606a6736f8ddd74a93e24572dad34ad9
Muhammad Gilang Nursyahroni <mgilangns.2710@gmail.com>
2021-04-23 17:39:52 +0700

    [Gilang] add Splash screen and home screen

--- MC1-Applied/Controller/ViewController.swift
+++ MC1-Applied/Controller/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        helloWorldLabel.text = "I'm Inside"
+        //helloWorldLabel.text = "I'm Inside"
     }
 
     
